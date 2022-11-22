11756355d0db3359d0370474c1fddaa7064a8947
Juan Carlos Martinez <j.c.m6110@gmail.com>
2022-11-22 13:22:03 -0600

    add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (143214323, 1, 'sleepy');
+
+--UPDATE sandbox SET double_value =  -12.324765 WHERE id = 140;
+
+--DELETE FROM sandbox WHERE id = 74;
+
+SELECT * FROM sandbox;
